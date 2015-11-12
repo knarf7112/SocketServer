6909935836789a03267b63d0d175dbda128886a8
knarf7112 <knarf7112@gmail.com>
2015-11-12 18:18:31 +0800

    #0010: 加入DebugToLog4Net的Listener,使Debug的輸出可以輸出到Log4Net

---
+++ SocketServer/DebugToLog4Net.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//
+using System.Diagnostics;
+using Common.Logging;
+
+namespace SocketServer
+{
+    /// <summary>
+    /// Debug的Listener轉到Common loggging(需要在app.config作設定監聽的物件)
+    /// <system.diagnostics>
+    //  <trace>
+    //    <listeners>
+    //      <add name="traceListener" type="Crypto.EskmsAPI_UnitTest.DebugToLog4Net, Crypto.EskmsAPI_UnitTest"></add>
+    //    </listeners>
+    //  </trace>
+    //</system.diagnostics>
+    /// </summary>
+    public class DebugToLog4Net :TraceListener
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(DebugToLog4Net));
+
+        public override void Write(string message)
+        {
+            log.Debug(m => m("{0}", message));
+        }
+
+        public override void WriteLine(string message)
+        {
+            log.Debug(m => m("{0}", message));
+        }
+    }
+}
--- WebSocket_Net45_Test/Program.cs
+++ WebSocket_Net45_Test/Program.cs
@@ -5,14 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 //
 using System.Net.WebSockets;
+using System.Threading;
 
 namespace WebSocket_Net45_Test
 {
     class Program
     {
+        //ref:http://readily-notes.blogspot.tw/
         static void Main(string[] args)
         {
             WebSocket sck;
+
+            Console.WriteLine(string.Format("main:{0}", Thread.CurrentThread.ManagedThreadId));
+
+            Task t = Task.Run(() => { Console.WriteLine(String.Format("Task:{0}", Thread.CurrentThread.ManagedThreadId)); });
+
+            qq();
+
+
+            Console.WriteLine("end ...");
+            Console.ReadKey();
+        }
+
+        static async Task GetAwaitThreadId()
+        {
+            Thread.Sleep(3000);
+            //Await方法執行緒ID
+            Console.WriteLine(string.Format("Await:{0}", System.Threading.Thread.CurrentThread.ManagedThreadId));
+        }
+        //async await 並不會產生新的執行緒
+        static async Task qq()
+        {
+            await GetAwaitThreadId();
         }
     }
 }
