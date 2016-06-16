deaff97ef3fe4c72c7a7627734e8bdf5a6de8fb7
knarf7112 <knarf7112@gmail.com>
2016-06-16 16:51:47 +0800

    #0047: 新增Apple手機的APNs訊息推送簡易測試

---
+++ Test_Apple_APNs_PushNotification/AppleNotificationPayload.cs
@@ -0,0 +1,148 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Apple_APNs_PushNotification
+{
+    public class AppleNotificationPayload
+    {
+        public NotificationAlert Alert { get; set; }
+
+        public int? ContentAvailable { get; set; }
+
+        public int? Badge { get; set; }
+
+        public string Sound { get; set; }
+
+        public bool HideActionButton { get; set; }
+
+        public Dictionary<string, object[]> CustomItems
+        {
+            get;
+            private set;
+        }
+
+        #region Constructor
+        public AppleNotificationPayload()
+        {
+            HideActionButton = false;
+            Alert = new NotificationAlert();
+            CustomItems = new Dictionary<string, object[]>();
+        }
+
+        public AppleNotificationPayload(string alert)
+        {
+            HideActionButton = false;
+            Alert = new NotificationAlert() { Body = alert };
+            CustomItems = new Dictionary<string, object[]>();
+        }
+
+        public AppleNotificationPayload(string alert, int badge)
+            : this(alert, badge, null)
+        {
+
+        }
+
+        public AppleNotificationPayload(string alert, int badge, string sound)
+		{
+			HideActionButton = false;
+			Alert = new NotificationAlert() { Body = alert };
+			Badge = badge;
+			Sound = sound;
+			CustomItems = new Dictionary<string, object[]>();
+		}
+        #endregion
+
+        public void AddCustom(string key, params object[] values)
+        {
+            if (values != null)
+            {
+                this.CustomItems.Add(key, values);
+            }
+        }
+
+        /// <summary>
+        /// convert to json format string
+        /// </summary>
+        /// <returns></returns>
+        public string ToJson()
+        {
+            JObject json = new JObject();
+
+            JObject aps = new JObject();
+
+            if (!this.Alert.IsEmpty)
+            {
+                if(!string.IsNullOrEmpty(this.Alert.Body) 
+                    && string.IsNullOrEmpty(this.Alert.LocalizedKey)
+                    && string.IsNullOrEmpty(this.Alert.ActionLocalizedKey)
+                    && (this.Alert.LocalizedArgs == null || this.Alert.LocalizedArgs.Count <= 0)
+                    && !this.HideActionButton)
+                {
+                    aps["alert"] = new JValue(this.Alert.Body);
+                }
+                else
+                {
+                    JObject jsonAlert = new JObject();
+
+                    if (!string.IsNullOrEmpty(this.Alert.LocalizedKey))
+                        jsonAlert["loc-key"] = new JValue(this.Alert.LocalizedKey);
+
+                    if (this.Alert.LocalizedArgs != null && this.Alert.LocalizedArgs.Count > 0)
+                        jsonAlert["loc-args"] = new JArray(this.Alert.LocalizedArgs.ToArray());
+
+                    if (!string.IsNullOrEmpty(this.Alert.Body))
+                        jsonAlert["body"] = new JValue(this.Alert.Body);
+
+                    if (this.HideActionButton)
+                        jsonAlert["action-loc-key"] = new JValue((string)null);
+                    else if (!string.IsNullOrEmpty(this.Alert.ActionLocalizedKey))
+                        jsonAlert["action-loc-key"] = new JValue(this.Alert.ActionLocalizedKey);
+
+                    aps["alert"] = jsonAlert;
+                    
+                }
+            }
+
+            if (this.Badge.HasValue)
+                aps["badge"] = new JValue(this.Badge.Value);
+
+            if (!string.IsNullOrEmpty(this.Sound))
+                aps["sound"] = new JValue(this.Sound);
+
+            if (this.ContentAvailable.HasValue)
+                aps["content-available"] = new JValue(this.ContentAvailable.Value);
+
+            json["aps"] = aps;
+
+            foreach (string key in this.CustomItems.Keys)
+            {
+                if (this.CustomItems[key].Length == 1)
+                    json[key] = new JValue(this.CustomItems[key][0]);
+                else if (this.CustomItems[key].Length > 1)
+                    json[key] = new JArray(this.CustomItems[key]);
+            }
+
+            string rawString = json.ToString(Newtonsoft.Json.Formatting.None, null);
+
+            StringBuilder encodedString = new StringBuilder();
+            foreach (char c in rawString)
+            {
+                if ((int)c < 32 || (int)c > 127)
+                    encodedString.Append("\\u" + string.Format("{0:x4}", Convert.ToUInt32(c)));
+                else
+                    encodedString.Append(c);
+            }
+
+            return rawString;// encodedString.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToJson();
+        }
+    }
+}
---
+++ Test_Apple_APNs_PushNotification/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.Net.Sockets;
+//
+using System.Net;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace Test_Apple_APNs_PushNotification
+{
+    class Program
+    {
+        static X509Certificate certificate;
+        static DateTime? Expiration;
+        static DateTime DoNotStore;
+        static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        static string DeviceToken = "c1564dd73cd73a003d2ad143d96c9e6d651f8b48b45ba8c0ae9c5db87513fde8";
+        //  private static string DeviceToken = "64d73e7b0815f7c22d7a412dbde7c4f4477ada6fdc4ee8d57db8ee536706dea6";
+        //ref:http://dbhills.blogspot.tw/2015/01/ciosapns.html
+        static void Main(string[] args)
+        {
+            /*
+            byte[] b1 = new byte[0];
+            byte[] b2 = new byte[] { };
+            DateTime d = DateTime.Now;
+            DateTime? date1 =  null;
+            DateTime date2 = default(DateTime);
+            var check = (date1 != date2);
+            var check2 = (date2.Equals(date1));
+            int data1 = IPAddress.HostToNetworkOrder(514);
+            byte[] aaa = BitConverter.GetBytes(data1);
+            */
+            AppleNotificationPayload payload = new AppleNotificationPayload();
+            payload.Alert.Body = "Test 國字來一下 Content";
+            payload.Badge = 3;
+            payload.Sound = "default";
+
+
+            string json = payload.ToJson();
+            Console.WriteLine("Send Json: " + json);
+
+            string sendUTF8Str = SendAPNS(DeviceToken, json);
+            Console.WriteLine("" + sendUTF8Str);
+            Console.ReadKey();
+        }
+
+        static string SendAPNS(string deviceToken, string content)
+        {
+            //ref:https://msdn.microsoft.com/en-us/library/txafckwd.aspx
+            //ref2:http://stackoverflow.com/questions/16101100/string-format-input-string-was-not-in-correct-format-for-string-with-curly-brack
+            //要多加雙括號"{{"才能讓參數寫入string的format
+            string jsonContent = String.Format("{{\"aps:\":{{\"alert\":\"{0}\",\"badge\":8,\"sound\":\"default\"}}}}",deviceToken);
+            //Json: { MID = 1000242, MsgID = 12345, RegID = "c1564dd73cd73a003d2ad143d96c9e6d651f8b48b45ba8c0ae9c5db87513fde8", Subj = "測試12 主題一:88個badge", Sum = 88, Title = "test2 Content" };
+            //str = "{\"aps\":{\"alert\":\"" + s2 + "\",\"badge\":10,\"sound\":\"default\"}}";
+            string hostIP = "gateway.sandbox.push.apple.com";//"gateway.push.apple.com";//feedback.sandbox.push.apple.com//"feedback.sandbox.push.apple.com";
+            int port = 2195;//2195;//2196
+            string password = "AllPay";//
+
+            //certificate load 需要去Apple申請App的憑證才有此檔
+            string certificatepath = "allpay_apns_dev.p12";// "aps_developer_allpay.p12";//bin/debug
+            string certificateFullPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AppleCertificate", certificatepath);
+
+            certificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(File.ReadAllBytes(certificateFullPath), password,X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+            var certificates = new System.Security.Cryptography.X509Certificates.X509Certificate2Collection();
+            certificates.Add(certificate);
+
+            //使用TCP Cient來建立connect(就是走簡易的http)
+            TcpClient apnsClient = new TcpClient();
+            Stopwatch timer = new Stopwatch();
+            try
+            {
+                timer.Start();
+                apnsClient.Connect(hostIP, port);
+            }
+            catch (SocketException ex)
+            {
+                timer.Stop();
+                Console.WriteLine("TimeSpend:{0}ms  ex:{1}", timer.ElapsedMilliseconds, ex.Message);
+            }
+            //主要認證憑證就是這段,可以使用event認證兩方的憑證,目前APNs這邊都只使用Apple給的憑證
+            System.Net.Security.SslStream apnsStream = new System.Net.Security.SslStream(apnsClient.GetStream(),
+                                                                                        false,
+                                                                                        new System.Net.Security.RemoteCertificateValidationCallback(ValidateServerCertificate),
+                                                                                        new System.Net.Security.LocalCertificateSelectionCallback(SelectLocalCertificate));
+
+            try
+            {
+                apnsStream.AuthenticateAsClient(hostIP, certificates, System.Security.Authentication.SslProtocols.Tls, false);
+                timer.Stop();
+                Console.WriteLine("做完認證的TimeSpend:{0}ms", timer.ElapsedMilliseconds);
+            }
+            catch(System.Security.Authentication.AuthenticationException ex)
+            {
+                Console.WriteLine("error:" + ex.Message);
+            }
+
+
+            if (!apnsStream.IsMutuallyAuthenticated)
+            {
+                Console.WriteLine("error:" + "Ssl Stream Failed to Authenticate");
+            }
+
+            if (!apnsStream.CanWrite)
+            {
+                Console.WriteLine("error:" + "Ssl Stream is not Writable");
+            }
+            //需要取得Apple手機給的token來當作裝置的識別碼,送的格式參考APPLE規定的JSON
+            byte[] message = ToBytes(deviceToken, content);
+            apnsStream.Write(message);//這邊就可以開始送資料了
+            return Encoding.UTF8.GetString(message);
+        }
+        //轉換成Apple指定的binary格式
+        //https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Appendixes/LegacyFormat.html#//apple_ref/doc/uid/TP40008194-CH105-SW1
+        static byte[] ToBytes(string deviceTokenHex,string content)
+        {
+            Console.WriteLine("ToBytes String: " + content);
+            //1. command => byte[]{ 0x01 }
+            byte[] command = new byte[]{ 0x01 };
+            //System.Threading.CountdownEvent s = new System.Threading.CountdownEvent(1);
+            
+            int identifier = 0;
+            //2.Identifier(2 bytes)
+            byte[] identifierBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(identifier));
+            int expiryTimeStamp = -1;
+            if (Expiration != DoNotStore)
+            {
+                DateTime concreteExpireDateUtc = (Expiration ?? DateTime.UtcNow.AddMonths(1).ToUniversalTime());
+                TimeSpan epochTimeSpan = concreteExpireDateUtc - UNIX_EPOCH;
+                expiryTimeStamp = (int)epochTimeSpan.TotalSeconds;
+            }
+            //3. Expiry(4 bytes)
+            byte[] expiry = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(expiryTimeStamp));
+
+            //5. DeviceToken(32 bytes)
+            byte[] deviceToken = new byte[DeviceToken.Length/2];
+            for(int i = 0; i < deviceToken.Length;i++)
+                deviceToken[i] = Byte.Parse(DeviceToken.Substring(i*2,2),System.Globalization.NumberStyles.HexNumber);
+
+            //4. DeviceToken Length(4 bytes)
+            byte[] deviceTokenSize = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(Convert.ToInt16(deviceToken.Length)));//因為為Big endian所以要顛倒
+
+            //7. Payload
+            byte[] payload = Encoding.UTF8.GetBytes(content);
+            //6. Payload Length
+            byte[] payloadSize = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(Convert.ToInt16(payload.Length)));//因為為Big endian所以要顛倒
+
+            List<byte[]> notificationParts = new List<byte[]>();
+            notificationParts.Add(command);
+            notificationParts.Add(identifierBytes);
+            notificationParts.Add(expiry);
+            notificationParts.Add(deviceTokenSize);
+            notificationParts.Add(deviceToken);
+            notificationParts.Add(payloadSize);
+            notificationParts.Add(payload);
+
+            return BuildBufferFrom(notificationParts);
+        }
+
+        static byte[] BuildBufferFrom(IList<byte[]> bufferParts)
+        {
+            int length = 0;
+            foreach (byte[] bytes in bufferParts)
+                length += bytes.Length;
+
+            byte[] result = new byte[length];
+            for (int i = 0, j = 0; i < bufferParts.Count; i++)
+            {
+                Buffer.BlockCopy(bufferParts[i], 0, result, j, bufferParts[i].Length);
+                j += (bufferParts[i].Length);
+            }
+
+
+            //int position = 0;
+            //for (int i = 0; i < bufferParts.Count; i++)
+            //{
+            //    byte[] part = bufferParts[i];
+            //    Buffer.BlockCopy(bufferParts[i], 0, result, position, part.Length);
+            //    position += part.Length;
+            //}
+
+            return result;
+        }
+
+        static byte[] HexToBytes(string hex)
+        {
+            Regex reg = new Regex("\b[0-9A-F]+",RegexOptions.IgnoreCase); 
+            if(!reg.IsMatch(hex))
+                throw new ArgumentException("have valid string");
+
+            byte[] result = new byte[hex.Length / 2];
+
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = Byte.Parse(hex.Substring(i * 2, 2), System.Globalization.NumberStyles.HexNumber);
+            }
+
+            return result;
+        }
+
+        //驗證Server端X509認證
+        static bool ValidateServerCertificate(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certificate, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            return true;// Dont care about server's cert
+        }
+
+        static X509Certificate SelectLocalCertificate(object sender, string targetHost, 
+            X509CertificateCollection localCertificates,
+            X509Certificate remoteCertificate, string[] acceptableIssuers)
+        {
+            return certificate;
+        }
+    }
+}
